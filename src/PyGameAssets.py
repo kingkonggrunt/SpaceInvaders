3ed34408cc0bc3a7812e674a3e1dfbca4b1b04c1
kingkonggrunt <kingkonggrunt@users.noreply.github.com>
2021-11-21 00:19:14 +1100

    fix: fix underscore issues to be inline with PEP8

--- src/PyGameAssets.py
+++ src/PyGameAssets.py
@@ -11,39 +11,39 @@ class PyGameImage:
 
     Attributes
     ----------
-    __x : int
+    x : int
         Current Asset's X coordinate
-    __y : int
+    y : int
         Current Asset's Y coordinate
-    __x_bound_lower : int
+    x_bound_lower : int
         Current Asset's Lower Bound X coordinate. To limit position of asset
-    __x_bound_upper : int
+    x_bound_upper : int
         Current Asset's Upper Bound X coordinate. To limit position of asset
-    __y_bound_lower : int
+    y_bound_lower : int
         Current Asset's Lower Bound Y coordinate. To limit position of asset
-    __y_bound_upper : int
+    y_bound_upper : int
         Current Asset's Upper Bound Y coordinate. To limit position of asset
     """
     def __init__(self, dir):
-        self.__image = pygame.image.load(dir)
+        self._image = pygame.image.load(dir)
 
-        self.__x = None
-        self.__y = None
+        self.x = None
+        self.y = None
 
-        self.__x_bound_lower = None
-        self.__x_bound_upper = None
-        self.__y_bound_lower = None
-        self.__y_bound_upper = None
+        self.x_bound_lower = None
+        self.x_bound_upper = None
+        self.y_bound_lower = None
+        self.y_bound_upper = None
 
     def start_coords(self, x, y):
-        self.__x = x
-        self.__y = y
+        self.x = x
+        self.y = y
 
     def render(self, screen, x=None, y=None):
         if x is None and y is None:
-            screen.blit(self.__image, (self.__x, self.__y))
+            screen.blit(self._image, (self.x, self.y))
         else:
-            screen.blit(self.__image, (x,y))
+            screen.blit(self._image, (x,y))
 
     def set_boundary(self, x_lower=None, x_upper=None, y_upper=None, y_lower=None):
         """
@@ -60,28 +60,28 @@ class PyGameImage:
         y_upper : int
             Upper Y coordinate boundary
         """
-        self.__x_bound_lower = x_lower
-        self.__x_bound_upper = x_upper
-        self.__y_bound_lower = y_lower
-        self.__y_bound_upper = y_upper
+        self.x_bound_lower = x_lower
+        self.x_bound_upper = x_upper
+        self.y_bound_lower = y_lower
+        self.y_bound_upper = y_upper
 
     def move(self, axis: bool, amount: int, ignore_boundaries=False):
         # axis == 1 is x axis
         if axis:
-            self.__x += amount
+            self.x += amount
         else:
-            self.__y += amount
+            self.y += amount
 
         if not ignore_boundaries:
-            if not self.__x_bound_lower is None:
-                if self.__x <= self.__x_bound_lower:
-                    self.__x = self.__x_bound_lower
-            if not self.__x_bound_upper is None:
-                if self.__x >= self.__x_bound_upper:
-                    self.__x = self.__x_bound_upper
-            if not self.__y_bound_lower is None:
-                if self.__y <= self.__y_bound_lower:
-                    self.__y = self.__y_bound_lower
-            if not self.__y_bound_upper is None:
-                if self.__y >= self.__y_bound_upper:
-                    self.__y = self.__y_bound_upper
+            if not self.x_bound_lower is None:
+                if self.x <= self.x_bound_lower:
+                    self.x = self.x_bound_lower
+            if not self.x_bound_upper is None:
+                if self.x >= self.x_bound_upper:
+                    self.x = self.x_bound_upper
+            if not self.y_bound_lower is None:
+                if self.y <= self.y_bound_lower:
+                    self.y = self.y_bound_lower
+            if not self.y_bound_upper is None:
+                if self.y >= self.y_bound_upper:
+                    self.y = self.y_bound_upper
