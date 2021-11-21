ff531fe88cc589ee71539229e5a05eeb77fcf422
kingkonggrunt <kingkonggrunt@users.noreply.github.com>
2021-11-22 10:11:03 +1100

    feat: fireable bullet

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ spaceship.start_coords(370, 480)
 spaceship.set_boundary(x_lower=0, x_upper=736)
 spaceship.speed = 0
 
+# Bullet
+_bullet = pygame.image.load("assets/001-bullet.png")
+bullet = PyGameImage("assets/001-bullet.png")
+bullet.start_coords(0, 480) # only needs to be off screen
+bullet.speed = -0.5
+bullet.state = False # Bullet is not fired yet
+bullet.start_x_on_fire = 490
+
 # Alien
 alien = PyGameImage("assets/001-alien-pixelated-shape-of-a-digital-game.png")
 alien.start_coords(randint(0,736), randint(50,150))
@@ -30,6 +38,11 @@ screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("SpaceInvaders")
 pygame.display.set_icon(icon)
 
+def fire_bullet(bullet, x, y):
+    bullet.state = True
+    screen.blit(_bullet, (x + 16), (y + 10))
+    # bullet.render(screen)
+
 ## Run Game
 running = True
 while running:
@@ -44,6 +57,12 @@ while running:
                 spaceship.speed = -0.3
             if event.key == pygame.K_RIGHT:
                 spaceship.speed = 0.3
+            if event.key == pygame.K_SPACE:
+                if bullet.state == False:
+                    bullet.state = True
+                    bullet.start_coords(spaceship.x + 16, bullet.start_x_on_fire)
+                    bullet.render(screen)
+                # fire_bullet(bullet, spaceship.x, bullet.y)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 spaceship.speed = 0
@@ -52,8 +71,6 @@ while running:
     screen.fill((255, 255, 255))
 
     ## Move and Draw Assets
-    background.move(axis=1, amount=random.normal(0, 0.009))
-    background.move(axis=0, amount=random.normal(0, 0.009))
     background.move(axis=1, amount=random.normal(0, 0.008))
     background.move(axis=0, amount=random.normal(0, 0.008))
 
@@ -72,6 +89,16 @@ while running:
     background.render(screen)
     spaceship.render(screen)
     alien.render(screen)
+
+    # Bullet movement
+    if bullet.y < 0 :
+        bullet.state = False
+        bullet.start_coords(0, bullet.start_x_on_fire)
+    if bullet.state is True:
+        bullet.start_coords(bullet.x, bullet.y)
+        bullet.render(screen)
+        bullet.y += bullet.speed
+
 
     pygame.display.update()
 
